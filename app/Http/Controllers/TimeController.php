a943f8faa8ce45e114d6b889f09416b5e89e89fc
AbdullahRahmoun <aboodyggkol@gmail.com>
2022-12-20 22:08:10 +0300

    added update time method and route

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Time;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class TimeController extends Controller
 {
@@ -69,7 +71,25 @@ class TimeController extends Controller
      */
     public function update(Request $request, Time $time)
     {
-        //
+        try{
+            $information=$request->validate([
+                'start' => 'required|date_format:H:i:s',
+                'end' => 'required|date_format:H:i:s|after:start',
+            ]);
+        }catch(ValidationException $e){
+            return response()->json(['msg'=>$e->getMessage()],401);
+        }
+        if($time->expert_id!=auth()->user()->id){
+            return response()->json([
+                'msg'=>"only account's owner can edit it's information"
+            ],422);
+        }
+        $time->update($information);
+        $reponse=[
+            'msg'=>'success',
+            'time'=>$time
+        ];
+        return $reponse;
     }
 
     /**
